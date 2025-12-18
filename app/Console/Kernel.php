3eb25ab7a23d4b5473d80b5447ef2158d925e2fa
murph <64770873+mrvrn@users.noreply.github.com>
2025-12-18 22:30:19 +0100

    Correct Scheduling for Laravel 12, added notes for Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,8 +7,18 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected function schedule(Schedule $schedule)
-    {
-        $schedule->command('app:run-schedules')->everyMinute();
-    }
+    // Since this project was created using Laravel 12, schedule doesn't use this file.
+    // The scheduling is done in routes/console.php.
+    // However, since this was mentioned in the instructions, I'm leaving the method here commented out.
+    // protected function schedule(Schedule $schedule)
+    // {
+    //     $schedule->command('app:run-schedules')->everyMinute();
+    // }
+
+    // protected function commands()
+    // {
+    //     $this->load(__DIR__.'/Commands');
+
+    //     require base_path('routes/console.php');
+    // }
 }
--- routes/console.php
+++ routes/console.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// When using a newer version of Laravel, you might need to schedule commands here instead.
-// use Illuminate\Support\Facades\Schedule;
-// Schedule::command('app:run-schedules')->everyMinute();
+
+// In Laravel 12, scheduling commands is done in routes/console.php.
+// So we schedule the command to run due schedules here.
+Schedule::command('app:run-schedules')->everyMinute();
